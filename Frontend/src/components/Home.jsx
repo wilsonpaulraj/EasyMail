a04991fd533c242876df813e1d46e89ae0438b45
Wilsonpaulraj <wilsonpaulrajd@gmail.com>
2024-10-07 07:46:05 +0530

    added some changes in the landing page

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -4,51 +4,86 @@ import "../customstyles.css";
 const Home = () => {
   return (
     <div className="overflow-x-hidden bg-background">
-      <div className=" min-h-screen max-w-full w-full px-24 py-10">
-        <div className="w-full flex justify-between">
+      <div className="min-h-screen w-full max-w-full px-24 py-10">
+        <div className="flex w-full justify-between">
           <div className="flex items-center">
-            <img className="h-10 w-10 mr-3" src="/logo.svg" alt="" />
-            <span className="font-bold text-primary text-3xl">Easy</span>
-            <span className="font-bold text-secondary text-3xl">Mail</span>
+            <img className="mr-3 h-10 w-10" src="/logo.svg" alt="" />
+            <span className="text-3xl font-bold text-primary">Easy</span>
+            <span className="text-3xl font-bold text-secondary">Mail</span>
           </div>
-          <div className="flex items-center justify-between w-64">
-            <span className=" underline text-blue-900 text-lg font-normal">Learn More</span>
-            <button className="rounded-sm border-0 bg-primary hover:scale-105 hover:bg-orange-700 hover:shadow-lg shadow-black transition ease-in-out duration-300 p-2 px-5 font-bold text-white ">GET STARTED</button>
+          <div className="flex w-64 items-center justify-between">
+            <span className="text-lg font-normal text-secondary underline">
+              Learn More
+            </span>
+            <button className="rounded-sm border-0 bg-primary p-2 px-5 font-bold text-white shadow-black transition duration-300 ease-in-out hover:scale-105 hover:bg-orange-700 hover:shadow-lg">
+              GET STARTED
+            </button>
           </div>
         </div>
-        <div className="flex justify-between w-full">
-          <div className="flex flex-col items-center justify-evenly flex-1 py-28 pr-5">
+        <div className="flex w-full justify-between">
+          <div className="flex flex-1 flex-col items-center justify-evenly py-28 pr-5">
             <div className="flex flex-col gap-2">
-              <p className="font-bold text-6xl font-lexend text-primary">Too busy to read all the e-mails?</p>
-              <p className="font-bold text-6xl font-lexend text-secondary">We got you!</p>
+              <p className="font-lexend text-6xl font-bold text-primary">
+                Too busy to read all the e-mails?
+              </p>
+              <p className="font-lexend text-6xl font-bold text-secondary">
+                We got you!
+              </p>
             </div>
             <div className="flex flex-col gap-6">
-              <p className="font-normal font-monsterrat w-9/12 text-xl text-wrap">
-                Effortlessly manage your inbox with EasyMail's smart summarization and organization tools.
-                click here to get started now
+              <p className="w-9/12 text-wrap font-monsterrat text-xl font-normal">
+                Effortlessly manage your inbox with EasyMail's smart
+                summarization and organization tools. click here to get started
+                now
               </p>
-              <button className="rounded-sm border-0 w-56 bg-primary hover:bg-orange-700 transition ease-in-out duration-300 p-2 px-5 font-bold text-white ">GET STARTED</button>
+              <button className="w-56 rounded-sm border-0 bg-primary p-2 px-5 font-bold text-white transition duration-300 ease-in-out hover:bg-orange-700">
+                GET STARTED
+              </button>
             </div>
           </div>
-          <div className="flex flex-1 items-center w-9/12 h-9/12">
+          <div className="h-9/12 flex w-9/12 flex-1 items-center">
             <img className="scale-90" src="/mails.png" alt="" />
           </div>
         </div>
-        <div className="flex flex-col gap-10 items-center justify-center">
-         
-          <div className="flex justify-center gap-5">
-
-           
-            <div className="rounded-xl w-96 h-[400px]">
-              
-
+        <div className="flex flex-col items-center justify-center gap-10">
+          <div className="flex flex-wrap justify-center gap-5">
+            <div className="flex w-96 flex-col items-center rounded-xl">
+              <img
+                className="h-80 w-80"
+                src="/email-classification.svg"
+                alt=""
+              />
+              <p className="w-10/12 text-wrap text-center font-monsterrat text-lg">
+                Effortlessly sort your emails into spam, promotions, and
+                essentials with just one click—let us do the heavy lifting for
+                you!
+              </p>
             </div>
-           
+            <div className="flex w-96 flex-col items-center rounded-xl">
+              <img
+                className="h-80 w-80"
+                src="/email-summarization.svg"
+                alt=""
+              />
+              <p className="w-10/12 text-wrap text-center font-monsterrat text-lg">
+                Don’t stress about the piles of emails in your inbox! Get a
+                quick summary of all your messages in just seconds!
+              </p>
+            </div>
+            <div className="flex w-96 flex-col items-center rounded-xl">
+              <img
+                className="h-80 w-80"
+                src="/email-prioritization.svg"
+                alt=""
+              />
+              <p className="w-10/12 text-wrap text-center font-monsterrat text-lg">
+                Wouldn't it be great if your inbox were organized and sorted?
+                Well, guess who's an expert at that! EasyMail expertly organizes
+                your inbox by prioritizing your most important emails.
+              </p>
+            </div>
           </div>
-
         </div>
-
-
       </div>
     </div>
   );
